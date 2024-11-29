14ddede6631744323ab82a013f27499bc8289279
Tamas Papp <161366386+Pooksyy@users.noreply.github.com>
2024-11-29 20:50:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -297,7 +297,7 @@ function updateIconClasses(clickedIcon) {
 
 document.addEventListener("DOMContentLoaded", () => {
   let today = new Date().getDate(); // Az aktuális nap száma
-  let month = new Date().getMonth() + 2;
+  let month = new Date().getMonth() + 1;
   console.log(month);
 
   setTimeout(() => {
